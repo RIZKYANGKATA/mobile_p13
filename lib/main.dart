d57332829694606b52808a89a34780c740f32d8f
RIZKYANGKATA <rizmadoris@gmail.com>
2023-12-10 00:46:19 +0700

    praktikum_5

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rizky Angkata',
+      title: 'Rizky Angkata Putra S.',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
